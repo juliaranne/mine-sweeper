e67d0357044befa101b68572cfaaab6e7d8fde42
julia-anne <julia.robinson@spareroom.co.uk>
2025-03-06 18:09:57 +0000

    start again

--- src/__tests__/index.test.js
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-import { createGrid } from "../index";
-
-test("expect a grid of given size with one bomb", () => {
-  const grid = createGrid(6);
-  expect(grid).toEqual([0, 1, 0, 0, 0, 0]);
-});
