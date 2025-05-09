375c3b00b2c052b2d50fd55e3adbdabd8f97f949
julia-anne <julia.robinson@spareroom.co.uk>
2025-05-09 15:58:43 +0100

    set up for single bomb

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,73 @@
 import "./style.css";
 
-class Bomb {
-  getLocation(gridsize: number): number {
-    return Math.floor(Math.random() * gridsize + 1);
+// grid size and bomb count
+// size squared array
+// generate random numbers less than array
+// split array by
+
+class BlockArray {
+  private gridArray: number[] = [];
+  private gridWidth: number;
+
+  constructor(s: number) {
+    this.gridWidth = s;
+  }
+
+  createGridArray() {
+    for (let i = 0; i < this.gridWidth; i += 1) {
+      const row = new Array(this.gridWidth).fill(0);
+      this.gridArray.push(row);
+    }
+    return this.gridArray;
+  }
+}
+
+class BombGrid {
+  // private bombLocation: number;
+  private blockCount: number;
+  private bombLocation: number;
+  private gridWidth: number;
+
+  constructor(s: number) {
+    this.gridWidth = s;
+    this.blockCount = s ** 2;
+    this.bombLocation = Math.floor(Math.random() * this.blockCount + 1);
+  }
+
+  addBombToGrid() {
+    const grid = new BlockArray(this.gridWidth);
+    const gridValues = grid.createGridArray();
+    let count = 0;
+    let bombSet = false;
+
+    for (let i = 0; i < this.gridWidth; i += 1) {
+      count += this.gridWidth;
+      if (count >= this.bombLocation && !bombSet) {
+        const bombIndex = this.bombLocation - (count - this.gridWidth);
+        gridValues[i][bombIndex - 1] = 9;
+        bombSet = true;
+      }
+    }
+    return gridValues;
   }
 }
 
 class Grid {
-  private size: number;
+  private dimension: number;
   private app: HTMLElement | null = document.getElementById("app");
 
-  constructor(s: number) {
-    this.size = s;
+  constructor(d: number) {
+    this.dimension = d;
   }
 
   setGridWidth(): void {
     if (this.app) {
-      this.app.style.width = `${42 * Math.sqrt(this.size)}px`;
+      this.app.style.width = `${42 * this.dimension}px`;
     }
   }
 
   drawGrid(): void {
-    for (let i = 0; i < this.size; i += 1) {
+    for (let i = 0; i < this.dimension ** 2; i += 1) {
       const block = new Block("plain");
       const btn = block.createBlock();
       this.app?.appendChild(btn);
@@ -48,6 +94,8 @@ class Block {
   }
 }
 
-const game = new Grid(9);
+const game = new Grid(3);
 game.setGridWidth();
 game.drawGrid();
+const grid = new BlockValues(3);
+console.log(grid.setBlockValues());
