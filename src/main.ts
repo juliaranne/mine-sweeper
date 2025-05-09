655a62c8ef301643120c862c2da8c0c0b04a8746
julia-anne <julia.robinson@spareroom.co.uk>
2025-05-09 13:07:04 +0100

    create block, grid and bomb objects

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,47 @@
 import "./style.css";
-import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    hello
-  </div>
-`;
+class Bomb {
+  getLocation(gridsize: number): number {
+    return Math.floor(Math.random() * gridsize + 1);
+  }
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+class Grid {
+  private size: number;
+
+  constructor(s: number) {
+    this.size = s;
+  }
+
+  drawGrid() {
+    const app = document.getElementById("app");
+    for (let i = 0; i < this.size; i += 1) {
+      const block = new Block("plain");
+      const btn = block.createBlock();
+      console.log(block, btn);
+      app?.appendChild(btn);
+    }
+  }
+}
+
+class Block {
+  private blockType: string;
+
+  constructor(bt: string) {
+    this.blockType = bt;
+  }
+
+  private isInPlay: boolean = true;
+
+  setIsInPlay(): void {
+    this.isInPlay = false;
+  }
+
+  createBlock(): HTMLButtonElement {
+    const btn = document.createElement("button");
+    return btn;
+  }
+}
+
+const game = new Grid(9);
+game.drawGrid();
