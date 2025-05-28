715097904052cf7ac62b1210f00204604d70a3bc
julia-anne <julia.robinson@spareroom.co.uk>
2025-05-28 16:35:29 +0100

    canvas size update

--- src/main-canvas.ts
+++ src/main-canvas.ts
@@ -1,6 +1,6 @@
-const canvasHeight = 100;
-const canvasWidth = 100;
-const tileSize = 20;
+const canvasHeight = 120;
+const canvasWidth = 120;
+const tileSize = 30;
 const tileCount = canvasWidth / tileSize;
 
 const canvas = document.getElementById("app") as HTMLCanvasElement;
@@ -10,9 +10,9 @@ function createRow(y: number) {
   for (let i = 0; i < tileCount; i += 1) {
     if (ctx) {
       ctx.fillStyle = "#ccc";
-      ctx.fillRect(i * tileSize, y, 20, 20);
+      ctx.fillRect(i * tileSize, y, 30, 30);
       ctx.strokeStyle = "black"; // Optional: Set the stroke color
-      ctx.strokeRect(i * tileSize, y, 20, 20);
+      ctx.strokeRect(i * tileSize, y, 30, 30);
     }
   }
 }
@@ -24,9 +24,9 @@ for (let j = 0; j < canvasHeight; j += tileSize) {
 function drawSquare(x: number, y: number) {
   if (ctx) {
     ctx.fillStyle = "#e0e0e0";
-    ctx.fillRect(x, y, 20, 20);
+    ctx.fillRect(x, y, 30, 30);
     ctx.strokeStyle = "white"; // Optional: Set the stroke color
-    ctx.strokeRect(x, y, 20, 20);
+    ctx.strokeRect(x, y, 30, 30);
   }
 }
 
@@ -34,8 +34,22 @@ canvas.addEventListener("mousedown", (e: MouseEvent) => {
   const x = e.offsetX;
   const y = e.offsetY;
 
-  console.log(y - (y % 20), x - (x % 20));
-  drawSquare(x - (x % 20), y - (y % 20));
+  console.log(y - (y % 30), x - (x % 30));
+  drawSquare(x - (x % 30), y - (y % 30));
 
   console.log(x, y);
 });
+
+function setBombLocation(x: number, y: number) {
+  if (ctx) {
+    ctx.beginPath();
+    ctx.arc(x, y, 12, 0, 2 * Math.PI, false);
+    ctx.fillStyle = "green";
+    ctx.fill();
+  }
+}
+
+setBombLocation(15, 15);
+
+// have sets of bomb coordinates, see if hit matches
+// have array of square coordinates? run through, count bombs
